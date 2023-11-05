74804a57a8f421e1330e55047cf05da55a6afa57
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-05 15:59:59 +0600

    API call useEffect style added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   return (
     <div className="App">
-     <Counter></Counter>
+     {/* <Counter></Counter> */}
+     <ExternalUsers></ExternalUsers>
     </div>
   );
+}
+
+function ExternalUsers(){
+  const [users, setUsers] = useState([]);
+  //useEffect (() =>{}, []);
+  useEffect (() =>{
+    //Data load korar jonno (jsonplaceholder.typicode.com/comment website theke (/users) site link copy kore bosay dibo)
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, []);
+  return(
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      { /*
+        users.map(users => <li>{users.name}</li>)
+  */}
+  {
+    users.map(users => <Users name={users.name} email={users.email}></Users>)
+  }
+
+    </div>
+  )
+}
+
+function Users(props){
+  return(
+    <div style={{border: '2px solid red', margin: '20px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
 }
 
 function Counter(){
